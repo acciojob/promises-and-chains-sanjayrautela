b0e50cec9f23d40fa7f2b2b50c78db7ffc910e59
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-02-26 12:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+document.getElementById('myForm').addEventListener('submit', function(event){
+  event.preventDefault();
+
+  var age = document.getElementById('age').value;
+  var name = document.getElementById('name').value;
+
+  if(!age || !name){
+    alert('please fill in all the fields');
+    return;
+  }
+
+  var promise = new Promise(function(resolve,reject){
+    setTimeout(function(){
+      if(age>18){
+        resolve();
+      }else{
+        reject();
+      }
+    },4000);
+  });
+
+  promise.then(function(){
+    alert('welcome,' + name + '.you can vote');
+  }).catch(function(){
+    alert('oh sorry' + name + 'you cnnot vote');
+  });
+
+
+})
